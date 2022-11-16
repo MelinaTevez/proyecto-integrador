5355e5cad2c7afa52b21785ff1104e48c614a78b
Melina Tevez <melinatevez15@gmail.com>
2022-11-16 11:00:02 -0300

    Trabajando en la validación del formulario de contacto

--- js/menu/contacto.js
+++ js/menu/contacto.js
@@ -1,27 +1,63 @@
 
-/*     regExpValidarContacto = [
-         /^[a-zA-ZÀ-ÿ\s]{1,40}$/ ,    // regexp nombre
-         /^[a-zA-ZÀ-ÿ\s]{1,40}$/,      // regexp apellido
-        /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,  // regexp email
-         /^\d{7,14}$/     // regexp telefono
-
- */
 function initContacto(){
     console.warn('initContacto()')
+    
+    const formContacto = document.getElementById('formulario-contacto')
+    console.log(formContacto)
+    const inputsContacto = document.querySelectorAll('#formulario-contacto input')
+    console.log(inputsContacto)
+    
+    const regExpValidarContacto = {
+            nombre:     /^[a-zA-ZÀ-ÿ\s]{2,25}$/ ,                            // regexp nombre
+            apellido:   /^[a-zA-ZÀ-ÿ\s]{2,25}$/,                             // regexp apellido
+            email:      /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,  // regexp email
+            telefono:    /^\d{7,14}$/                                         // regexp telefono
+    
+    }
 
-    const inputName = document.getElementById('nombre-contacto')
-    const inputApellido = document.getElementById('apellido')
-    const inputEmail = document.getElementById('email')
-    const inputTelefono = document.getElementById('telefono')
-    const formContacto = document.getElementsByClassName('form-contacto')
-    const pWarning = document.getElementsByClassName('form-contacto__warning')
+    const validarForm = (e) => {
+        switch(e.target.name) {
+            case 'nombre':
+                validarCampo(regExpValidarContacto.nombre, e.target, 'nombre');
+                break;
+            case 'apellido':
+                validarCampo(regExpValidarContacto.apellido, e.target, 'apellido');
+            break;
+            case 'email':
+                validarCampo(regExpValidarContacto.email, e.target, 'email');
+            break;
+            case 'telefono':
+                validarCampo(regExpValidarContacto.telefono, e.target, 'telefono');
+            break;
+        }
+    } 
 
-/* 
-    console.log(inputName, inputApellido, inputEmail, inputTelefono) */
-/*     formContacto.addEventLister('submit', e => {
-        e.preventDefault()
+    const validarCampo = (expresion, input, campo) => {
 
-      
+        if(expresion.test(input.value)){
+
+            console.log('ES VALIDO')
+           /*  document.getElementById(`grupo__${campo}`).classList.remove('form-contacto__info-invalido')
+            document.getElementById(`grupo__${campo}`).classList.add('form-contacto__info-valido') */
+            
+          } else{
+              console.log('ES INVALIDO')
+             /*  document.getElementById(`grupo__${campo}`).classList.add('form-contacto__info-invalido')
+              document.getElementById(`grupo__${campo}`).classList.remove('form-contacto__info-valido')
+
+              document.querySelector(`#grupo__${campo} .form-contacto__input-error'`).classList.add('form-contacto__input-error--activo') */
+             
+        }
+    }
+    inputsContacto.forEach((input)=> {
+        input.addEventListener('keyup', validarForm)
+        input.addEventListener('blur', validarForm)
+        
     })
-} */
+    formContacto.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+
+    })
+
 }
